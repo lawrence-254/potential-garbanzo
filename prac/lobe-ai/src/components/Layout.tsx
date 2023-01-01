cc76cde8c40470558c7ef16ee6437323f34db19e
lawrence-254 <lodhiambo26@gmail.com>
2023-01-02 00:47:15 +0300

    propsintroduction

--- prac/lobe-ai/src/components/Layout.tsx
+++ prac/lobe-ai/src/components/Layout.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import styled from 'styled-components';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const LayoutContainer = styled.div`
 display: flex;
 flex-direction:column;
 align-items:center;
+`;
+const InnerLayoutContainer = styled.div`
+padding: 0px,2px;
+max-width: 70%
 `
 
-function Layout() {
+function Layout(props:{
+    children : React.ReactNode
+}) {
   return (
-    <LayoutContainer>Layout</LayoutContainer>
+    <LayoutContainer>
+        <Navbar/>
+        <InnerLayoutContainer>
+            {props.children}
+        </InnerLayoutContainer>
+        <Footer/>
+    </LayoutContainer>
   )
 }
 

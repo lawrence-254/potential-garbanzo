1d7494b9afac2849760235f50a1a69cb61396ddf
lawrence-254 <lodhiambo26@gmail.com>
2023-01-01 03:17:40 +0300

    pycodestyle

--- py/dsa-in-py/array/insert_end.py
+++ py/dsa-in-py/array/insert_end.py
@@ -8,6 +8,7 @@ inserts an item at the end of an array
 def insert_at_the_end(arr, item):
     arr.append(item)
 
+
 if __name__ == '__main__':
     odd = [1, 3, 5, 7, 9]
     no = 11
@@ -15,3 +16,4 @@ if __name__ == '__main__':
 
     insert_at_the_end(odd, no)
     print('after:', odd)
+    

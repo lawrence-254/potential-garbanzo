748cd34cbb24ffae18befb9a8c5a40d4be754afb
lawrence-254 <lodhiambo26@gmail.com>
2023-01-01 03:13:26 +0300

    0

--- py/dsa-in-py/array/insert_any_pos.py
+++ py/dsa-in-py/array/insert_any_pos.py
@@ -3,7 +3,17 @@
 a function that inserts an item at any position in an array
 '''
 
+
 def insert_at_any_point(array, num_to_insert, index_to_insert_to, arr_size):
     '''
     new array
     '''
+    # arr_size = len(array)
+    # while (arr_size > index_to_insert_to):
+    for i in range(arr_size - 1, index_to_insert_to-1, -1):
+        array[i + 1] = array[i]
+
+    array[index_to_insert_to]
+    
+ if __name__ == '__main__':
+    pass

9c3e893b140cc5342bb2fb3b163a00395b75b88c
lawrence-254 <lodhiambo26@gmail.com>
2023-01-01 22:05:33 +0300

    layoutSetup

--- prac/lobe-ai/src/components/Layout.tsx
+++ prac/lobe-ai/src/components/Layout.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from 'styled-components';
 
 const LayoutContainer = styled.div`
+display: flex;
+flex-direction:column;
+align-items:center;
 `
 
 function Layout() {

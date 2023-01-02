e571c002e8bab16ecc0602a620b19e168fc31460
lawrence-254 <lodhiambo26@gmail.com>
2023-01-02 11:41:06 +0300

    testlayout

--- prac/lobe-ai/src/components/Layout.tsx
+++ prac/lobe-ai/src/components/Layout.tsx
@@ -7,10 +7,13 @@ const LayoutContainer = styled.div`
 display: flex;
 flex-direction:column;
 align-items:center;
+width: 100vw;
+outline: 1px solid  #04DDB2
 `;
 const InnerLayoutContainer = styled.div`
 padding: 0px,2px;
 max-width: 70%
+outline: 1px solid #04DDB2
 `
 
 function Layout(props:{

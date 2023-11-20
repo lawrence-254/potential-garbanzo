3f0d00a77904d73aa3ed8c1759a44af1c83f8b96
lawrence-254 <lodhiambo26@gmail.com>
2023-11-20 11:56:43 +0300

    cleanup

---
+++ step_2/e-shop/resources/js/components/AddToCart.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <hr class="my-4">
+        <button type="button" class="btn btn-warning rounded-pill px-4 py-2 transition duration-300 hover bg-transparent border border-2 border-warning text-warning">
+            Add to Cart
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>

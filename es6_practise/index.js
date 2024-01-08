098ccc81913a281345a58587c63709648657ca5b
lawrence-254 <lodhiambo26@gmail.com>
2024-01-08 11:16:12 +0300

    length function

---
+++ es6_practise/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+/**
+ * length methods
+ */
+const fruits = [];
+fruits.push('orange', 'banana', 'apple')
+console.log(fruits);
+console.log(fruits.length);
+console.log('end sec 1');
+fruits[5] = "peach";
+console.log(fruits);
+console.log(fruits.length);
+console.log(Object.keys(fruits));
+console.log('end sec 2');
+fruits.length = 10;
+console.log(fruits);
+console.log(fruits.length);
+console.log(Object.keys(fruits));
+console.log('end sec 3');
+fruits.length = 2;
+console.log(fruits);
+console.log(fruits.length);
+console.log(Object.keys(fruits));
+console.log('end sec 4');

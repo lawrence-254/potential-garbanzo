5e7cbcbd829432ee3f997ec7e8ae305700d87973
lawrence-254 <lodhiambo26@gmail.com>
2024-01-15 10:33:20 +0300

    counter

---
+++ advanced/countasync.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+python script that demonstrates how asyncio works
+'''
+import asyncio
+
+async  def count():
+    print('one')
+    await asyncio.sleep(2)
+    print('two')
+
+async def main():
+    await asyncio.gather(count(), count(), count())
+
+if __name__ == "__main__":
+    import time
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds")

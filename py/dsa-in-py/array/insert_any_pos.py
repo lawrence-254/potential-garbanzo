d56ba4cfc25eafaa0a314980c9c8bbe6216271f8
lawrence-254 <lodhiambo26@gmail.com>
2024-01-20 12:04:57 +0300

    insert at any point

---
+++ py/dsa-in-py/array/insert_any_pos.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+a function that inserts an item at any position in an array
+'''
+
+def insert_at_any_point(array, num_to_insert, index_to_insert_to, arr_size):
+    '''
+    new array
+    '''

a6e3a98a7b2e0a4b74a42928423826f567ffb37a
lawrence-254 <lodhiambo26@gmail.com>
2024-02-10 15:28:19 +0300

    init

---
+++ advanced/rest/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5001")
